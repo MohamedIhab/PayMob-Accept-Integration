dbc19885367c2fd8979db6dab9cc409f97aa2bed
Mohamed Ihab <mohamedihabtaher@gmail.com>
2017-07-11 15:07:27 +0200

    initial commit

---
+++ PayMob-Accept-Integration-Python/__init__.py
@@ -0,0 +1,6 @@
+from .paymob_accept import authentication_token_request
+from .paymob_accept import create_order
+from .paymob_accept import generate_payment_key
+from .paymob_accept import upload_iframe
+from .paymob_accept import render_iframe
+from .paymob_accept import transaction_response
---
+++ PayMob-Accept-Integration-Python/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='paymob_accept',
+      version='0.1',
+      description='Easy integration with Accept API offered by PayMob',
+      url='https://github.com/MohamedIhab/PayMob-Accept-Integration',
+      author='Mohamed Ihab',
+      author_email='mohamedihab@paymobsolutions.com',
+      license='MIT',
+      packages=['paymob_accept'],
+      zip_safe=False)
